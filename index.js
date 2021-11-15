695da5804fe1653eb31ed2e8de3a9ed67323162a
Divik Babbar <91770907+Code19Master@users.noreply.github.com>
2021-11-15 18:40:37 +0000

    Added kill command

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ let embed = new Discord.MessageEmbed()
 .addField("GAME COMMANDS:video_game:",
 '`--rps`' )
 .addField("FUN COMMANDS",
-'`--rr`, ' + '`--hack`,' + '`--coinflip`, ' + '`--roast`, ' + '`--fact`, ' + '`--say`')
+'`--rr`, ' + '`--hack`,' + '`--coinflip`, ' + '`--roast`, ' + '`--fact`, ' + '`--say`, ' + '`--kill`')
 .addField("MODERATION COMMANDS",
 '`--kick`, ' + '`--ban`, ' + '`--purge`, ' + '`--mute`, ' + '`--unmute`, ' + '`--membercount`, ' + '`--serverinfo`')
 .addField("TECHNOLOGY COMMANDS:man_technologist:",
@@ -310,6 +310,24 @@ if(message.content === "--serverinfo") {
  message.channel.send(`${content}`)
  }
 }
+//kill
+ if(message.content.startsWith("--kill")) {
+ let victim = message.mentions.users.first()
+ if(!victim) message.reply("Mention someone to Kill")
+ else{
+let replies = [ (`${victim} has been shot`), (`${victim} has been stabbed`), (`${victim} has been drowned`), 
+ (`${victim} has been electrified`), (`A goose honked at ${victim} to death`), 
+ (`Some psychopath zapped ${victim} with his laser eyes`), 
+ (`${victim} ate a poisonous potato`), (`${victim} died from slowmode being to long`), 
+ (`${victim} was run over by car`), (`${victim} was pushed in lava`), (`${victim} was banned by the server owner`), 
+ (`${victim} was found dead in a dumpster`), 
+ (`Someone named Joe was found chewing on ${victim}'s leg`), (`${victim} got drunk and fell in the water`), 
+ (`${victim} made a deal with the devil`), (`${victim} was hacked by an 
+Oreo`), (`An alien named MEE6 abducted ${victim} in their sleep`),]
+
+ message.channel.send(`${replies[Math.floor(Math.random() * replies.length)]}`) 
+ }
+ }
 });
 
 
